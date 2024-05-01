ddad491c3531c18e6fe88651f55bf51fe922608c
Durgaprasad272004 <158829667+Durgaprasad272004@users.noreply.github.com>
2024-05-01 10:43:48 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import java.applet.*;
+/*
+<applet code="Demo.class" width="1000" height="1000"> </applet>
+*/
+public class Demo extends Applet {
+public void paint(Graphics g) {
+g.drawLine(20,40,100,300);
+}
+}
